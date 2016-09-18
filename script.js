afefe57531fadec6f9540c29ceba80a083fda1f7
sherri010 <aminshahidi.sh@gmail.com>
2016-09-17 19:14:12 -0700

    player can move now

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var ground =[[1,1,0,1,1,1],
 //second player :computer
 //for player have x and y to work as indxes in grid
 
+
 $(document).ready(function(){
 
 //CREATING GAME OBJECT
@@ -27,7 +28,7 @@ Game.prototype.init=function(){
    for(var i=0 ; i< 6 ; i++){
       for(var j=0 ; j<6 ; j++){
           status = (this.ground[i][j] === 1)? "on":"off";
-          this.board.append("<div class='cell "+status+"'></div>");
+          this.board.append("<div class='cell "+status+"' id='"+i+j+"'></div>");
       }
     }
 
@@ -37,53 +38,125 @@ Game.prototype.init=function(){
 Game.prototype.addPlayers=function(finder){
   // this.players.push(runner);
    this.players.push(finder);
-  $($('#wrapper div:first-child')).attr("id",finder.id);
+  var elementClassList= $('div:eq(1)').attr('class');
+  elementClassList = elementClassList.concat(" "+finder.id);
+  $('div:eq(1)').attr('class',elementClassList);
+}
 
+/// CREATING OBJECT TO HOLD POSITION DATA FOR EACH MOVE, LIKE A TEMP VAR
+function CurrenPosition(p){
+   this.x=p.x;
+   this.y=p.y;
 }
 
 
+//CREATING PLAYER OBJECT
 function Player(setting){
    this.img=setting.img;
    this.name=setting.name;
    this.id=setting.id;
+   this.x=setting.x;
+   this.y=setting.y;
 
 }
-//add action listener on players
 
+//add EVENT listener on players
 Player.prototype.setReady=function(){
         console.log("initializing player...");
         var p=this;
       $('body').keydown(function(e){
         p.repostion(e.which);
     });
-
+    console.log('firing event listener...');
 }
 
 Player.prototype.repostion=function(key){
+var before,after,identifier,swap_temp;
  switch(key){
    case 38:
-     console.log("up");
+       //console.log("up");
+     if(game.ground[p1Current.x-1][p1Current.y] === 1){
+
+         //where player is now
+          identifier=p1Current.x.toString()+p1Current.y.toString();
+          before =document.getElementById(identifier);
+          //where it's going
+          p1Current.x-=1
+          identifier = p1Current.x.toString()+ (p1Current.y).toString();
+          after = document.getElementById(identifier);
+          swap_temp =  before.getAttribute('class');
+          before.setAttribute('class', swap_temp.slice(0,swap_temp.length-7));
+          after.setAttribute('class', after.getAttribute('class')+" "+p1.id);
+     }
+
      break;
   case 40:
-    console.log("down");
+    //  console.log("down");
+    if(game.ground[p1Current.x+1][p1Current.y] === 1){
+
+        //where player is now
+         identifier=p1Current.x.toString()+p1Current.y.toString();
+         before =document.getElementById(identifier);
+         //where it's going
+         p1Current.x+=1
+         identifier = p1Current.x.toString()+ (p1Current.y).toString();
+         after = document.getElementById(identifier);
+         swap_temp =  before.getAttribute('class');
+         before.setAttribute('class', swap_temp.slice(0,swap_temp.length-7));
+         after.setAttribute('class', after.getAttribute('class')+" "+p1.id);
+    }
+
+
     break;
   case 37:
-    console.log("left");
+   //  console.log("left");
+   if(game.ground[p1Current.x][p1Current.y-1] === 1){
+
+       //where player is now
+        identifier=p1Current.x.toString()+p1Current.y.toString();
+        before =document.getElementById(identifier);
+        //where it's going
+        p1Current.y-=1
+        identifier = p1Current.x.toString()+ (p1Current.y).toString();
+        after = document.getElementById(identifier);
+        swap_temp =  before.getAttribute('class');
+        before.setAttribute('class', swap_temp.slice(0,swap_temp.length-7));
+        after.setAttribute('class', after.getAttribute('class')+" "+p1.id);
+
+
+   }
     break;
   case 39:
-    console.log("right");
+    // console.log("right");
+     if(game.ground[p1Current.x][p1Current.y+1] === 1){
+
+        //where player is now
+         identifier=p1Current.x.toString()+p1Current.y.toString();
+         before =document.getElementById(identifier);
+         //where it's going
+         p1Current.y+=1
+         identifier = p1Current.x.toString()+ (p1Current.y).toString();
+         after = document.getElementById(identifier);
+         swap_temp =  before.getAttribute('class');
+         before.setAttribute('class', swap_temp.slice(0,swap_temp.length-7));
+         after.setAttribute('class', after.getAttribute('class')+" "+p1.id);
+
+   }
+   else { console.log('blocked')}
     break;
   default:
     break;
 
  }
 }
-// I can check my sibilings -
+
 /////////////////////////////////////
 var game = new Game({ board:$('#wrapper'), g:ground});
-var p1= new Player({img:"super.gif", name:"man", id:"finder" });
+var p1= new Player({img:"super.gif", name:"man", id:"finder",x:0,y:0 });
+var p1Current = new CurrenPosition(p1);
+
 game.init();
-p1.setReady();
+p1.setReady(game);
 game.addPlayers(p1);
 
 });
